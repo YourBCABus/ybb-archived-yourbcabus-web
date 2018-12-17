9d12d56e8965ef25b28091eeff5f47f2431de204
Anthony Li <li.anthony917@gmail.com>
2018-12-16 22:45:01 -0500

    Add bus searching
    Feature parity with [MyBCABus](https://mybcabus.com)! :tada:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgxWebstorageModule } from 'ngx-webstorage';
 
@@ -21,6 +22,7 @@ import { BusLocationComponent } from './bus-location/bus-location.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
     FontAwesomeModule,
     NgxWebstorageModule.forRoot({})
   ],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { StarredBusesService } from '../starred-buses.service';
+import { FormControl } from '@angular/forms';
+import { Bus } from '../interfaces';
+import { debounceTime } from 'rxjs/operators';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'ybb-home',
@@ -10,6 +14,30 @@ import { StarredBusesService } from '../starred-buses.service';
 export class HomeComponent implements OnInit {
 
   public isIOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+  public searchIcon = faSearch;
+  public searchField = new FormControl("");
+  public buses: Bus[] = [];
+  public starredBuses: Bus[] = [];
+  public debounceTime = 100;
+
+  busFilter(searchTerm: string) {
+    let processed = searchTerm.trim().toLowerCase();
+    return (bus: Bus) => {
+      return (bus.name && bus.name.toLowerCase().includes(processed)) || (bus.locations && bus.locations.findIndex(location => location.toLowerCase() === processed) >= 0);
+    };
+  }
+
+  refreshBuses() {
+    let searchTerm = this.searchField.value as string;
+    if (searchTerm && searchTerm.trim().length > 0) {
+      let filter = this.busFilter(searchTerm);
+      this.buses = this.api.buses.value.filter(filter);
+      this.starredBuses = this.starManager.starredBuses.value.filter(filter);
+    } else {
+      this.buses = this.api.buses.value;
+      this.starredBuses = this.starManager.starredBuses.value;
+    }
+  }
 
   constructor(
     public api: ApiService,
@@ -17,6 +45,21 @@ export class HomeComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.api.buses.subscribe(() => {
+      this.refreshBuses();
+    });
+
+    this.starManager.starredBuses.subscribe(() => {
+      this.refreshBuses();
+    });
+
+    this.searchField.valueChanges.pipe(debounceTime(this.debounceTime)).subscribe(() => {
+      this.refreshBuses();
+    });
+  }
+
+  trackBus(bus: Bus) {
+    return bus._id;
   }
 
 }
