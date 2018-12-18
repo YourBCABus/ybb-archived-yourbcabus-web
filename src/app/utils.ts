b9fd64a0db527e6cf14475e116b81a70091aa431
Anthony Li <li.anthony917@gmail.com>
2018-12-17 19:43:27 -0500

    So it was a 2-char bug, and I was wrong.

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -2,5 +2,5 @@ import { isDevMode } from '@angular/core';
 import { Bus } from './interfaces';
 
 export function isBusValidated(bus: Bus, date: Date = new Date()) {
-  return isDevMode() || (!bus.invalidate_time || bus.invalidate_time < date);
+  return (!bus.invalidate_time || bus.invalidate_time > date);
 }
